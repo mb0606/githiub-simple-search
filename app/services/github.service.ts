9097e0bcc981ba8d1baf3911110bad702f1aba10
Marco Berardini <mberardini@evolution-man.com>
2016-12-06 14:02:09 -0800

    [client] Inject Http, created function for getting profile

--- app/services/github.service.ts
+++ app/services/github.service.ts
@@ -2,11 +2,23 @@ import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '../config.keys'
+
 
 @Injectable()
 
 export class GithubService{
   private username:string;
-  private client_id = 'd904b9206da7b642d0ff';
-  private client_secret = 'b0e28e130c21a86ead9592308fa2261dfd43449d';
+  private client_id = 'GITHUB_CLIENT_ID';
+  private client_secret = 'GITHUB_CLIENT_SECRET';
+
+  constructor(private _http: Http) {
+    console.log('Github Service is ready');
+    this.username = 'mb0606'
+  }
+
+  getUser(){
+    return this._http.get('http://api.github.com/users/' + this.username + '?client_id=' + this.client_id )
+      .map(res => res.json());
+  }
 }
